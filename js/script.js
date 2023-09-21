b89861a001c9b4fcc06aee53e2824d032bbbe0b1
Josué Yovani <josue.yovani@outlook.com>
2023-09-21 16:41:56 -0600

    Add function render one post

--- js/script.js
+++ js/script.js
@@ -7,7 +7,88 @@ const searchButton = document.querySelector("#searchButton");
 const containerListPosts = document.querySelector("#list-posts");
 
 /* Functions */
+const renderTag = (hashTag, container) => {
+  const tag = document.createElement("li");
+  tag.className = "card__tag me-2";
+  tag.textContent = hashTag;
+  container.appendChild(tag);
+};
+const renderListTags = (listTags, tag) => {
+  listTags.forEach((element) => {
+    renderTag(element, tag);
+  });
+};
+
 const renderPost = (dataPost) => {
+  // elements tags
+  const listTags = document.createElement("ul");
+  listTags.className = "card__tags d-flex flex-wrap list-unstyled";
+  renderListTags(dataPost.tags, listTags);
+  // elment description
+  const cardDescription = document.createElement("p");
+  cardDescription.className = "card-text";
+  cardDescription.textContent = dataPost.description;
+  // element title
+  const cardTitle = document.createElement("h5");
+  cardTitle.className = "card-title";
+  cardTitle.textContent = dataPost.title;
+
+  // elements author
+  const imgAuthor = document.createElement("img");
+  imgAuthor.src = dataPost.author.avatar;
+  imgAuthor.alt = dataPost.author.name;
+
+  const anchorCommentImage = document.createElement("a");
+  anchorCommentImage.className = "card__comment--image--final d-flex";
+  anchorCommentImage.appendChild(imgAuthor);
+
+  const authorImage = document.createElement("div");
+  authorImage.className = "card__comment--image";
+  authorImage.appendChild(anchorCommentImage);
+
+  const anchorAuthorName = document.createElement("a");
+  anchorAuthorName.textContent = dataPost.author.name;
+  const anchorAuthorDate = document.createElement("a");
+  anchorAuthorDate.textContent = dataPost.date_post;
+
+  const authorNameDate = document.createElement("div");
+  authorNameDate.className = "name__date--container d-flex flex-column";
+  authorNameDate.appendChild(anchorAuthorName);
+  authorNameDate.appendChild(anchorAuthorDate);
+
+  const cardAuthorContainer = document.createElement("div");
+  cardAuthorContainer.className = "card__top--photo-name d-flex flex-row";
+  cardAuthorContainer.appendChild(authorImage);
+  cardAuthorContainer.appendChild(authorNameDate);
+
+  const cardAuthor = document.createElement("div");
+  cardAuthor.className = "card__top--content d-flex flex-row";
+
+  cardAuthor.appendChild(cardAuthorContainer);
+
+  // element card body
+  const cardBody = document.createElement("div");
+  cardBody.className = "card-body";
+
+  cardBody.appendChild(cardAuthor);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardDescription);
+  cardBody.appendChild(listTags);
+
+  // element image post
+  const imgPost = document.createElement("img");
+  imgPost.className = "card-img-top";
+  imgPost.src = dataPost.img_url;
+  imgPost.alt = dataPost.title;
+
+  // element card
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.appendChild(imgPost);
+  card.appendChild(cardBody);
+
+  containerListPosts.append(card);
   console.log(dataPost);
 };
 
