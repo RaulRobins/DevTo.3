2db37ea0f0efd81c46b6373ec432f9274357ae8b
Noe Castro <noec5298@gmail.com>
2023-09-20 20:45:37 -0500

    estructura de crear post

---
+++ pages/createPost/script.js
@@ -0,0 +1,33 @@
+const URL = 'https://js-2a847-default-rtdb.firebaseio.com/'; 
+const publishButton = document.querySelector("#Enviar");
+
+
+
+const createPost = async(post) => {
+    //codigo que se ejecute  por default
+    const url = URL + '.json';
+     const create = await fetch(url , { 
+      method: 'POST', 
+       headers: { 'Content-type': 'application/json:charset=UTF-8'}, 
+      body: JSON.stringify(post),
+     });   
+     /* if(create.status === 200){
+      getinfo()
+     } */
+  };
+
+publishButton.addEventListener("click", () => {
+    const coverIMG = document.querySelector("#file");
+    const newPostTitle = document.querySelector("#newPostTitle");
+    const postContent = document.querySelector("#postcontent")
+    const post = {
+      IMG: coverIMG.value, 
+      TitlePost: newPostTitle.value,
+      ContentPost: postContent.value,
+
+    }
+  createPost(post)
+  console.log(post)
+  
+ 
+  });
